fefca2e762209543a164f7025511e98975c1e6ac
Steven Dalfall <steven.dalfall@live.se>
2025-04-05 17:03:00 +0200

    Adjust dockerfiles

--- stevensoftware.client/Dockerfile.vue
+++ stevensoftware.client/Dockerfile.vue
@@ -1,9 +1,5 @@
 FROM node:18 AS build
 WORKDIR /app
-RUN apt-get update && apt-get install -y git
-ARG REPO_URL=https://github.com/lordstimpa/StevenSoftware.git
-RUN git clone $REPO_URL . && cd stevensoftware.client
-
 COPY package.json package-lock.json ./
 RUN npm install
 RUN npm run build
