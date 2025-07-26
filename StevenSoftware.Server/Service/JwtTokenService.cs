7f6525c87989e691ed0c1e152867e1d58a49a3ea
Steven Dalfall <steven.dalfall@live.se>
2025-07-26 12:15:02 +0200

    Add logging capabilities

--- StevenSoftware.Server/Service/JwtTokenService.cs
+++ StevenSoftware.Server/Service/JwtTokenService.cs
@@ -11,16 +11,18 @@ namespace StevenSoftware.Server.Services
 	{
 		private readonly IConfiguration _config;
 		private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ILogger<JwtTokenService> _logger;
 
-		public JwtTokenService(IConfiguration configuration, UserManager<ApplicationUser> userManager)
+        public JwtTokenService(IConfiguration configuration, UserManager<ApplicationUser> userManager, ILogger<JwtTokenService> logger)
 		{
 			_config = configuration;
 			_userManager = userManager;
-		}
+            _logger = logger;
+        }
 
 		public JwtTokenDto CreateTokenAsync(ApplicationUser user)
 		{
-			var claims = new[]
+            var claims = new[]
 			{
 				new Claim(JwtRegisteredClaimNames.Sub, user.Email),
 				new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
@@ -40,7 +42,8 @@ namespace StevenSoftware.Server.Services
 			);
 
 			var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
-			return new JwtTokenDto { AccessToken = tokenString };
+
+            return new JwtTokenDto { AccessToken = tokenString };
 		}
 	}
 }
